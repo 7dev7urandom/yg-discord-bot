b6b3cc6107892727e0a48580f8d801e14728ab57
7dev7urandom <micah2004h@icloud.com>
2021-02-01 20:26:07 +0700

    better filter

--- index.ts
+++ index.ts
@@ -100,7 +100,7 @@ try{
         }
 
         if(!(message.guild === mainGuild)) return;
-        if(message.content.toLowerCase().includes("the way") || message.content.toLowerCase() === 'way' || message.content.toLowerCase().includes("wae") || message.content.toLowerCase().includes("waye") || message.content.toLowerCase().includes("wai")) {
+        if(message.content.replace(/\W/g, '').toLowerCase().includes("theway") || message.content.toLowerCase() === 'way' || message.content.toLowerCase().includes("wae") || message.content.toLowerCase().includes("waye") || message.content.toLowerCase().includes("wai") || message.content.toLowerCase().includes("wë")) {
             message.delete();
             message.reply("Nice try you bag of lobster whiskers");
             return;
