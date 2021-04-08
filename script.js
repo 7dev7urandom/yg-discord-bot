895438b8699ea846045204da159638548bbd219f
Micah <micah2004h@icloud.com>
2021-04-08 16:48:20 +0700

    get rid of old changes

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ const { Client } = require('discord.js');
 
 const client = new Client();
 
-client.on('message', (message) => {
-    if(!(message.channel.type == 'dm' && message.author.id == '694538295010656267')) return;
+client.on('message', async (message) => {
+    if(!(message.channel.type == 'dm' && message.author.id == '710696257768652802')) return;
     (await message.channel.messages.fetch()).forEach((message, id) => {
         console.log(message.author.username + ": " + message.content);
     });
     process.exit();
 })
 
-client.login('NzgyODUwNDM2MDQ2NjUxNDAy.X8SMSA.TzEpv0ULe5dziwN6g73KDCglYRs').then(async () => {
+client.login('NzgyODUwNDM2MDQ2NjUxNDAy.X8SMSA.rXtcvmAprR-4dk_jqrEcRgLtxDU')
+/*
+	.then(async () => {
     const user = await client.users.fetch('694538295010656267');
     console.log(JSON.stringify(user));
     const messages = await user.dmChannel.messages.fetch();
@@ -18,4 +20,4 @@ client.login('NzgyODUwNDM2MDQ2NjUxNDAy.X8SMSA.TzEpv0ULe5dziwN6g73KDCglYRs').then
         console.log(message.author.username + ": " + message.content);
     });
 });
-
+*/
